8f93cd1b14b2fc4bc09e478d6af76d4144618d89
RechieKho <the.real.richie.kho@gmail.com>
2024-03-16 20:49:55 +0800

    Add `data` module.

---
+++ include/data.h
@@ -0,0 +1,23 @@
+#ifndef CONTAINER_DATA_H
+#define CONTAINER_DATA_H
+
+#include <stddef.h>
+
+/// A piece of data.
+typedef struct data_t
+{
+    void *ptr;
+    size_t size;
+} data_t;
+
+/// Shorthand literal.
+#define DATA_NEW(mp_ptr, mp_size) \
+    (data_t) { .ptr = (mp_ptr), .size = (mp_size) }
+
+/// Cast the `mp_data.ptr` pointer to given `id_type` type.
+/// Results `id_ref` variable that points to `mp_data.ptr` (a double pointer).
+/// `id_ref` is `NULL` when the size is not the same.
+#define DATA_CAST(id_type, id_ref, mp_data) \
+    id_type **const id_ref = sizeof(id_type) == (mp_data).size ? (id_type **)&(mp_data).ptr : NULL
+
+#endif // CONTAINER_DATA_H
