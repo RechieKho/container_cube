3e0d8116a76f45e9f402e370209eff789bee157f
RechieKho <the.real.richie.kho@gmail.com>
2024-03-16 20:49:20 +0800

    Specialize.

--- include/addition.h
+++
@@ -1,6 +0,0 @@
-#ifndef CRAB_ADDITION_H
-#define CRAB_ADDITION_H
-
-int add(int a, int b);
-
-#endif // CRAB_ADDITION_H
--- source/bin/hello_world.c
+++ source/bin/hello_world.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <addition.h>
 
-int main() {
+int main()
+{
     printf("Hello world.\n");
-    printf("Add 1 and 2 produce %d.\n", add(1, 2));
     return 0;
 }
