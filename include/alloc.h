35be9309ab85b3a48fdca9b60159a84cf60ae2f4
RechieKho <the.real.richie.kho@gmail.com>
2024-03-17 18:10:48 +0800

    Rename `alloca` module to `alloc`.

--- include/alloc.h
+++ include/alloc.h
@@ -1,10 +1,14 @@
-#ifndef CONTAINER_ALLOCA_H
-#define CONTAINER_ALLOCA_H
+#ifndef CONTAINER_ALLOC_H
+#define CONTAINER_ALLOC_H
 
 #include <stdlib.h>
 
 /// Shorthand allocation.
-#define ALLOC(id_type) \
+#define ALLOC(mp_ptr, mp_size) \
+    realloc((mp_ptr), (mp_size))
+
+/// Shorthand typed allocation.
+#define ALLOC_TYPED(id_type) \
     (id_type *)calloc(1, sizeof(id_type))
 
 /// Shorthand free.
