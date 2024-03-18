a93cc2a5c98c284662a86b99fae5ce1241e0561b
RechieKho <the.real.richie.kho@gmail.com>
2024-03-18 13:50:45 +0800

    Append error location when raise error.

--- include/cow.h
+++ include/cow.h
@@ -3,6 +3,7 @@
 
 #include <stdbool.h>
 #include <data.h>
+#include <macros.h>
 
 /// Copy-On-Write data.
 typedef struct cow_t
@@ -26,11 +27,11 @@ typedef struct cow_t
 /// Set `mp_status` to `mp_message` (a c-string error message) and go to `id_label`,
 /// when `mp_condition` evaluate to true.
 /// It is used to raise error.
-#define RAISE(mp_status, id_label, mp_condition, mp_message) \
-    if (mp_condition)                                        \
-    {                                                        \
-        (mp_status) = COW(false, mp_message, sizeof(char));  \
-        goto id_label;                                       \
+#define RAISE(mp_status, id_label, mp_condition, mp_message)                                                         \
+    if (mp_condition)                                                                                                \
+    {                                                                                                                \
+        (mp_status) = COW(false, mp_message " [in " __FILE__ ", line " MACRO_STRINGIFY(__LINE__) "]", sizeof(char)); \
+        goto id_label;                                                                                               \
     }
 
 /// Set result of `mp_expression` to `mp_status`.
